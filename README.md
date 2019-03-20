d6838de18a06a81ca01922fbd3d1089a81934095
dwyanwede <shang_sk@163.com>
2019-03-20 17:43:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+spring boot 整合dubbo注解方式实现
---
+++ spring-boot-dubbo/src/main/java/com/sxs/cn/spring/boot/SpringBootDubboApplication.java
@@ -0,0 +1,13 @@
+package com.sxs.cn.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDubboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDubboApplication.class, args);
+	}
+
+}
